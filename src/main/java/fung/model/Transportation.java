d2835959221955f762cce5f82c2cb03e4c1afbe0
LaoBing_Fung <FLB_B401@163.com>
2019-02-17 11:40:55 +0800

    add setter and getter method for price

--- src/main/java/fung/model/Transportation.java
+++ src/main/java/fung/model/Transportation.java
@@ -3,5 +3,13 @@ package main.java.fung.model;
 public abstract class Transportation {
     private double price;
 
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public abstract boolean isAvaliable(double cost);
 }
